1bff0233eb26d7efeba3fc97e286c21e866ecb6d
pear_c <zxcv9676@naver.com>
2025-04-25 10:55:21 +0900

    [feat] Transaction 관리 구현 완료

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/transaction/DbConnectionThreadLocal.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/transaction/DbConnectionThreadLocal.java
@@ -1,8 +1,10 @@
 package com.nhnacademy.shoppingmall.common.mvc.transaction;
 
+import com.nhnacademy.shoppingmall.common.util.DbUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 @Slf4j
 public class DbConnectionThreadLocal {
@@ -11,12 +13,20 @@ public class DbConnectionThreadLocal {
 
     public static void initialize(){
 
-        //todo#2-1 - connection pool에서 connectionThreadLocal에 connection을 할당합니다.
+        try {
+            //todo#2-1 - connection pool에서 connectionThreadLocal에 connection을 할당합니다.
+            Connection conn = DbUtils.getDataSource().getConnection();
+            connectionThreadLocal.set(conn);
 
-        //todo#2-2 connectiond의 Isolation level을 READ_COMMITED를 설정 합니다.
+            //todo#2-2 connectiond의 Isolation level을 READ_COMMITED를 설정 합니다.
+            conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 
-        //todo#2-3 auto commit 을 false로 설정합니다.
+            //todo#2-3 auto commit 을 false로 설정합니다.
+            conn.setAutoCommit(false);
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static Connection getConnection(){
@@ -32,14 +42,32 @@ public class DbConnectionThreadLocal {
     }
 
     public static void reset(){
+        Connection conn = getConnection();
 
-        //todo#2-4 사용이 완료된 connection은 close를 호출하여 connection pool에 반환합니다.
+        if(conn == null) {
+            return;
+        }
 
-        //todo#2-5 getSqlError() 에러가 존재하면 rollback 합니다.
-
-        //todo#2-6 getSqlError() 에러가 존재하지 않다면 commit 합니다.
-
-        //todo#2-7 현제 사용하고 있는 connection을 재 사용할 수 없도록 connectionThreadLocal을 초기화 합니다.
-
+        try {
+            //todo#2-5 getSqlError() 에러가 존재하면 rollback 합니다.
+            if (getSqlError()) {
+                conn.rollback();
+            } //todo#2-6 getSqlError() 에러가 존재하지 않다면 commit 합니다.
+            else {
+                conn.commit();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                //todo#2-4 사용이 완료된 connection은 close를 호출하여 connection pool에 반환합니다.
+                getConnection().close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            //todo#2-7 현제 사용하고 있는 connection을 재 사용할 수 없도록 connectionThreadLocal을 초기화 합니다.
+            connectionThreadLocal.remove();
+            sqlErrorThreadLocal.remove();
+        }
     }
 }
