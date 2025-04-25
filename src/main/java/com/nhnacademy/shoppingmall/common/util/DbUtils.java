406ae171fe840f1e4def9d6febd30b57c00848c7
pear_c <zxcv9676@naver.com>
2025-04-25 10:15:00 +0900

    [feat] DbUtil 설정

--- src/main/java/com/nhnacademy/shoppingmall/common/util/DbUtils.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/util/DbUtils.java
@@ -23,23 +23,25 @@ public class DbUtils {
         }
 
         //todo#1-1 {ip},{database},{username},{password} 설정
-
+        basicDataSource.setUrl("jdbc:mysql://10.116.64.14:13306/nhn_academy_17");
+        basicDataSource.setUsername("nhn_academy_17");
+        basicDataSource.setPassword("e(kbT!u88]ReeHwL");
 
         //todo#1-2 initialSize, maxTotal, maxIdle, minIdle 은 모두 5로 설정합니다.
-
+        basicDataSource.setInitialSize(5);
+        basicDataSource.setMaxTotal(5);
+        basicDataSource.setMaxIdle(5);
+        basicDataSource.setMinIdle(5);
 
         //todo#1-3 Validation Query를 설정하세요
-
-
         basicDataSource.setMaxWait(Duration.ofSeconds(2));
+        basicDataSource.setValidationQuery("select 1");
 
         //todo#1-4 적절히 변경하세요
-        DATASOURCE = null;
-
+        DATASOURCE = basicDataSource;
     }
 
     public static DataSource getDataSource(){
         return DATASOURCE;
     }
-
 }
