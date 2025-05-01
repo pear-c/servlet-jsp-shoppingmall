15c22b77e289e8ed5e9a08994541e933a211bf2b
pear_c <zxcv9676@naver.com>
2025-05-01 16:56:45 +0900

    [feat] products, categories 테이블 생성

--- submit/erd/script.sql
+++ submit/erd/script.sql
@@ -10,3 +10,24 @@ CREATE TABLE `users` (
     `latest_login_at` datetime DEFAULT NULL COMMENT '마지막 로그인 일자',
     PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='회원';
+
+-- category 테이블
+CREATE TABLE categories(
+     category_id int AUTO_INCREMENT NOT NULL COMMENT '카테고리 구분키',
+     category_name varchar(50) NOT NULL COMMENT '카테고리 명',
+     PRIMARY KEY (category_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='카테고리';
+
+-- product 테이블
+CREATE TABLE products(
+     product_id int AUTO_INCREMENT NOT NULL COMMENT '상품번호',
+     category_id int NOT NULL COMMENT '카테고리 ID',
+     product_name varchar(100) NOT NULL COMMENT '상품명',
+     product_price int NOT NULL COMMENT '상품가격',
+     product_created_at datetime NOT NULL COMMENT '상품등록일',
+     product_image_path varchar(255) COMMENT '상품 이미지 경로',
+     product_explain text NOT NULL COMMENT '상품설명',
+     PRIMARY KEY(product_id),
+     CONSTRAINT fk_products_category FOREIGN KEY(category_id) REFERENCES categories(category_id)
+     ON DELETE RESTRICT
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='상품';
