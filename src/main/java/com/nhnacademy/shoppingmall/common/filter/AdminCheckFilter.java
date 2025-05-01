fe3639cdab0320cb0d51c6af68506927c62fa7bc
pear_c <zxcv9676@naver.com>
2025-05-01 11:34:37 +0900

    [feat] AdminCheckFilter 수정

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/AdminCheckFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/AdminCheckFilter.java
@@ -19,9 +19,9 @@ public class AdminCheckFilter extends HttpFilter {
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#11 /admin/ 하위 요청은 관리자 권한의 사용자만 접근할 수 있습니다. ROLE_USER가 접근하면 403 Forbidden 에러처리
         HttpSession session = req.getSession();
-        User user = (User) req.getAttribute("user");
+        User user = (User) session.getAttribute("loginUser");
 
-        if(Objects.isNull(session) || user.getUserAuth().equals(User.Auth.ROLE_USER)) {
+        if(user.getUserAuth().equals(User.Auth.ROLE_USER)) {
             res.sendError(HttpServletResponse.SC_FORBIDDEN, "관리자 권한만 접근 가능");
         }
     }
